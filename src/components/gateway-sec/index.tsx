a0d5e60162ea3a79f8382d1a30e5e02b7f7c3b65
nihalnaveed12 <nihalnaveed50@gmail.com>
2025-05-05 21:06:36 +0500

    updated

--- src/components/gateway-sec/index.tsx
+++ src/components/gateway-sec/index.tsx
@@ -1,36 +1,46 @@
 "use client";
 import Image from "next/image";
-import { Button } from "../ui/button";
-import {motion} from "framer-motion"
+import { motion } from "framer-motion";
+import Link from "next/link";
 
 export default function GatewaySec() {
   return (
     <div className="relative flex lg:flex-row flex-col gap-6 px-4">
       <div className="flex gap-10 flex-col lg:w-[30%] w-full justify-center lg:items-start items-center ">
-        <motion.h3 
-        initial={{ opacity: 0, x:-100 }}
-        whileInView={{
-          opacity: 2,
-          x: 0,
-          transition: { duration: 0.6 },
-        }}
-        className="text-[16px] text-[#0A71B8]">Incredible Escapes</motion.h3>
-        <motion.h1 
-        initial={{ opacity: 0, x:-100 }}
-        whileInView={{
-          opacity: 2,
-          x: 0,
-          transition: { duration: 0.6 },
-        }}
-        className="text-5xl font-sans font-extrabold">
+        <motion.h3
+          initial={{ opacity: 0, x: -100 }}
+          whileInView={{
+            opacity: 2,
+            x: 0,
+            transition: { duration: 0.6 },
+          }}
+          className="text-[16px] text-[#0A71B8]"
+        >
+          Incredible Escapes
+        </motion.h3>
+        <motion.h1
+          initial={{ opacity: 0, x: -100 }}
+          whileInView={{
+            opacity: 2,
+            x: 0,
+            transition: { duration: 0.6 },
+          }}
+          className="text-5xl font-sans font-extrabold"
+        >
           Must see <br /> Getaways
         </motion.h1>
-        <Button className="bg-[#0A71B8] hover:bg-[#0A71B8] rounded-[6px] px-6 py-4 lg:w-[60%] lg:h-[15%]  cursor-pointer">
-          Discover Now
-        </Button>
+        <Link
+          className="bg-[#0A71B8] hover:bg-[#0A71B8] flex items-center justify-center text-zinc-50 text-center rounded-[6px] px-6 py-4 lg:w-[60%] lg:h-[15%]  cursor-pointer"
+          href={"/destinations"}
+        >
+          <button className="cursor-pointer">Discover Now</button>
+        </Link>
       </div>
       <div className="flex md:flex-row flex-col gap-5 md:w-[70%]  w-full mx-auto">
-        <div className="overflow-hidden  rounded-[20px] relative">
+        <Link
+          href={"/destinations/blue-lagoon-akamas"}
+          className="overflow-hidden  rounded-[20px] relative"
+        >
           <Image
             src={"/images/gateway1.jpg"}
             alt="Paphos Sea"
@@ -38,9 +48,14 @@ export default function GatewaySec() {
             width={1000}
             className="transition-transform hover:scale-110 duration-500 object-cover relative"
           />
-          <h2 className="absolute bottom-2 px-7 text-2xl font-sans  text-white">Blue Lagoon - Akamas</h2>
-        </div>
-        <div className="overflow-hidden border  rounded-[20px] relative">
+          <h2 className="absolute bottom-2 px-7 text-2xl font-sans  text-white">
+            Blue Lagoon - Akamas
+          </h2>
+        </Link>
+        <Link
+          href={"/destinations/peyia-destination"}
+          className="overflow-hidden border  rounded-[20px] relative"
+        >
           <Image
             src={"/images/gateway2.jpg"}
             alt="Paphos Sea"
@@ -48,8 +63,10 @@ export default function GatewaySec() {
             width={1000}
             className="transition-transform hover:scale-110 duration-500 object-cover "
           />
-          <h2 className="absolute bottom-2 px-7 text-2xl font-sans  text-white">Peyia Destinations</h2>
-        </div>
+          <h2 className="absolute bottom-2 px-7 text-2xl font-sans  text-white">
+            Peyia Destinations
+          </h2>
+        </Link>
       </div>
 
       <div
--- src/data/destinations.ts
+++ src/data/destinations.ts
@@ -1,32 +1,44 @@
 export const destinations = [
-    {
-      title: "Blue Lagoon - Akamas",
-      desc: "Blue Lagoon offers a unique perspective of the beautiful landscape that defines the region.",
-      src: "/destination-img/destination.jpeg",
-      href: "/destinations/blue-lagoon-akamas",
-      maindesc: "Your journey begins at the charming Latchi Harbor, ensuring a more intimate and personalized experience. Escape the crowds and set sail on a luxury cruise that starts from this hidden gem on the western coast of Cyprus. You will be treated to exceptional service that will make your Capo Greco Cruise truly memorable.",
-      maindesc2: "Dive into the refreshing waters of the Blue Lagoon and explore the vibrant marine life beneath the surface. Our expert guides will lead you to the best snorkeling spots, allowing you to witness the beauty of the underwater world.",
-      maindesc3: "EJoin us on a breathtaking cruise that promises to be a feast for your senses. Immerse yourself in the beauty of the Akamas Peninsula, where crystal-clear waters and untouched landscapes create the perfect backdrop for an extraordinary adventure.",
-      maindesc4: "Sail through the stunning Akamas Peninsula, home to the world-renowned Blue Lagoon. Be mesmerized by the tranquil turquoise waters and pristine beaches that define this slice of paradise."
-    },
-    {
-      title: "Peyia Destinations",
-      desc: "Uncover the Mysteries: Sea Caves Adventure in Peyia, St. Georges Island and the crystal-clear waters of Coral Bay. Learn More",
-      src: "/destination-img/dest3.jpg",
-      href: "/destinations/peyia-destinations",
-      maindesc: "Prepare to be enchanted as you explore the hidden wonders of the Peyia destinations  a natural masterpiece nestled along the rugged coastline of Cyprus. Our Peyia Adventure promises an immersive journey into a world where natures artistry and the Mediterraneans allure converge to create an unforgettable experience.",
-      maindesc2: "Witness the extraordinary beauty of nature as you navigate through intricate rock formations, sculpted by the relentless embrace of the sea. The Sea Caves of Peyia are a geological marvel, boasting arches, tunnels, and chambers that evoke a sense of wonder and awe.",
-      maindesc3: "Enjoy this adventure in comfort aboard our modern and well-equipped cruise vessel. Whether you prefer sunbathing on deck or seeking shade in the cozy lounge area, our cruise ensures a relaxed and enjoyable experience.",
-      maindesc4: "Dont miss the opportunity to discover the secrets of the Sea Caves in Peyia, St. Georges Island, and Coral Bay. Book your adventure today for a journey that combines natural wonders, exploration, and relaxation. Whether you are a nature enthusiast, an avid snorkeler, or someone seeking tranquility by the sea, our Sea Caves Adventure is tailor-made for you."
-    },
-    {
-      title: "Rikkos Bay",
-      desc: "Explore Rikkos Bay, a perfect blend of natural beauty, relaxation, and water activities.",
-      src: "/destination-img/dest2.jpg",
-      href: "/destinations/rikkos-bay",
-      maindesc: "Escape the hustle and bustle and discover the untouched beauty of Rikkos Bay. Tucked away from the crowds, this hidden paradise offers a serene and intimate setting for those seeking tranquility along the Mediterranean coast.",
-      maindesc2: "Dive into the crystal-clear waters and explore the vibrant marine life that calls Rikkos Bay home. Snorkel amidst colorful fish and discover the underwater wonders that make this bay a haven for nature enthusiasts and water adventurers alike.",
-      maindesc3: "End your day in awe as the sun dips below the horizon, casting a warm glow over Rikkos Bay. Our sunset views are nothing short of magical, providing the perfect backdrop for romantic moments or quiet reflections.",
-      maindesc4: "Indulge in the ultimate coastal retreat at Rikkos Bay, where the gentle lull of the waves and the caress of the sea breeze create an atmosphere of pure relaxation."
-    },
-  ];
+  {
+    title: "Blue Lagoon - Akamas",
+    desc: "Blue Lagoon offers a unique perspective of the beautiful landscape that defines the region.",
+    src: "/destination-img/destination.jpeg",
+    href: "/destinations/blue-lagoon-akamas",
+    maindesc:
+      "Your journey begins at the charming Latchi Harbor, ensuring a more intimate and personalized experience. Escape the crowds and set sail on a luxury cruise that starts from this hidden gem on the western coast of Cyprus. You will be treated to exceptional service that will make your Capo Greco Cruise truly memorable.",
+    maindesc2:
+      "Dive into the refreshing waters of the Blue Lagoon and explore the vibrant marine life beneath the surface. Our expert guides will lead you to the best snorkeling spots, allowing you to witness the beauty of the underwater world.",
+    maindesc3:
+      "EJoin us on a breathtaking cruise that promises to be a feast for your senses. Immerse yourself in the beauty of the Akamas Peninsula, where crystal-clear waters and untouched landscapes create the perfect backdrop for an extraordinary adventure.",
+    maindesc4:
+      "Sail through the stunning Akamas Peninsula, home to the world-renowned Blue Lagoon. Be mesmerized by the tranquil turquoise waters and pristine beaches that define this slice of paradise.",
+  },
+  {
+    title: "Peyia Destinations",
+    desc: "Uncover the Mysteries: Sea Caves Adventure in Peyia, St. Georges Island and the crystal-clear waters of Coral Bay. Learn More",
+    src: "/destination-img/dest3.jpg",
+    href: "/destinations/peyia-destinations",
+    maindesc:
+      "Prepare to be enchanted as you explore the hidden wonders of the Peyia destinations  a natural masterpiece nestled along the rugged coastline of Cyprus. Our Peyia Adventure promises an immersive journey into a world where natures artistry and the Mediterraneans allure converge to create an unforgettable experience.",
+    maindesc2:
+      "Witness the extraordinary beauty of nature as you navigate through intricate rock formations, sculpted by the relentless embrace of the sea. The Sea Caves of Peyia are a geological marvel, boasting arches, tunnels, and chambers that evoke a sense of wonder and awe.",
+    maindesc3:
+      "Enjoy this adventure in comfort aboard our modern and well-equipped cruise vessel. Whether you prefer sunbathing on deck or seeking shade in the cozy lounge area, our cruise ensures a relaxed and enjoyable experience.",
+    maindesc4:
+      "Dont miss the opportunity to discover the secrets of the Sea Caves in Peyia, St. Georges Island, and Coral Bay. Book your adventure today for a journey that combines natural wonders, exploration, and relaxation. Whether you are a nature enthusiast, an avid snorkeler, or someone seeking tranquility by the sea, our Sea Caves Adventure is tailor-made for you.",
+  },
+  {
+    title: "Rikkos Bay",
+    desc: "Explore Rikkos Bay, a perfect blend of natural beauty, relaxation, and water activities.",
+    src: "/destination-img/dest2.jpg",
+    href: "/destinations/rikkos-bay",
+    maindesc:
+      "Escape the hustle and bustle and discover the untouched beauty of Rikkos Bay. Tucked away from the crowds, this hidden paradise offers a serene and intimate setting for those seeking tranquility along the Mediterranean coast.",
+    maindesc2:
+      "Dive into the crystal-clear waters and explore the vibrant marine life that calls Rikkos Bay home. Snorkel amidst colorful fish and discover the underwater wonders that make this bay a haven for nature enthusiasts and water adventurers alike.",
+    maindesc3:
+      "End your day in awe as the sun dips below the horizon, casting a warm glow over Rikkos Bay. Our sunset views are nothing short of magical, providing the perfect backdrop for romantic moments or quiet reflections.",
+    maindesc4:
+      "Indulge in the ultimate coastal retreat at Rikkos Bay, where the gentle lull of the waves and the caress of the sea breeze create an atmosphere of pure relaxation.",
+  },
+];
